32f5dc9ee8e85951cdbfa7f8cc6ab450a600330b
Shaun Ek <shaune@providentmetals.com>
2019-06-19 02:09:23 -0500

    Clean up views
    Eliminate uneccessary serializers, etc.

--- smilewidgets/products/urls.py
+++ smilewidgets/products/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
     path('get-price/', views.fetch_price),
 ]
--- smilewidgets/products/views.py
+++ smilewidgets/products/views.py
@@ -3,42 +3,22 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from products.models import Product, ProductPrice, GiftCard
-from products.serializers import ProductSerializer, ProductPriceSerializer
 from datetime import datetime
 from collections import namedtuple
 
-def product_list(request):
-    """
-    List all products
-    """
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-def product_detail(request, id):
-    """
-    List product details
-    """
-    try:
-        product = Product.objects.get(pk=id)
-    except Product.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = ProductSerializer(product)
-        return JsonResponse(serializer.data)
-
 @csrf_exempt
+@api_view(['POST'])
 def fetch_price(request):
     """
     Gets product pricing, taking into account date and gift card data
     """
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        product_code, date = ('', '')
         try:
+            # Product cod and date are required
             product_code = data['productCode']
             date = data['date']
             date = datetime.strptime(date, '%Y-%m-%d')
@@ -48,7 +28,7 @@ def fetch_price(request):
             product = Product.objects.get(code=product_code)
         except Product.DoesNotExist:
             return HttpResponse(status=404)
-
+        # Gift card code field is optional
         gift_card_code = data['giftCardCode'] if 'giftCardCode' in data else None
 
         product_price = ProductPrice.objects.filter(
@@ -63,13 +43,15 @@ def fetch_price(request):
             code=gift_card_code
         ).order_by('-date_start').first()
         
-        price_output = {}
-        price_output['discount'] = gift_code.amount if gift_code is not None else 0
-        price_output['originalPrice'] = product_price.price \
-                                          if product_price is not None \
-                                          else product.price
-        price_output['price'] = product_price.price - price_output['discount'] \
-                                    if product_price is not None \
-                                    else product.price - price_output['discount']
+        discount = gift_code.amount if gift_code is not None else 0
+        original_price = product_price.price \
+                            if product_price is not None \
+                            else product.price
+        price = original_price - discount \
+                    if original_price >= discount \
+                    else original_price
 
-        return JsonResponse(price_output)
+        # Use a simple dictionary for output instead of a more typical object output with serializer
+        price_output = {'price':price, 'originalPrice': original_price, 'discount': discount}
+        
+        return Response(price_output)
